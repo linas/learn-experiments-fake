f9d21e9ea74183151cb4498eb8fe9b19f5f44028
Linas Vepštas <linasvepstas@gmail.com>
2021-03-12 12:33:20 -0600

    More processing

--- expt-9/all.sh
+++ expt-9/all.sh
@@ -6,7 +6,7 @@
 #
 # ----------------------
 
-# Load config parameters
+# Load master config parameters
 if [ -z $MASTER_CONFIG_FILE ]; then
 	echo "MASTER_CONFIG_FILE not defined!"
 	exit -1
@@ -19,6 +19,8 @@ else
 	exit -1
 fi
 
+# ----------------------
+# Step two - pair counting
 if ! [ -z ${PAIR_CONF_FILE} ] && [ -r ${PAIR_CONF_FILE} ]; then
 	. ${PAIR_CONF_FILE}
 else
@@ -46,6 +48,7 @@ sleep 1
 guile -s ${COMMON_DIR}/marginals-pair.scm
 
 # ------------------------
+# Step three - MST parsing and disjunct counting
 if ! [ -z ${MST_CONF_FILE} ] && [ -r ${MST_CONF_FILE} ]; then
 	. ${MST_CONF_FILE}
 else
@@ -53,7 +56,7 @@ else
 	exit -1
 fi
 
-# Copy the database, to provide iolation between stages.
+# Copy the database, to provide isolation between stages.
 if [[ $STORAGE_NODE = "(RocksStorageNode"* ]]; then
 	cp -pr ${PAIRS_DB} ${MST_DB}
 elif [[ $STORAGE_NODE = "(PostgresStorageNode"* ]]; then
@@ -82,5 +85,44 @@ sleep 1
 # Compute the disjunct marginals.
 guile -s ${COMMON_DIR}/marginals-mst.scm
 
-echo yoa quit
+# ------------------------
+# Step four - Clustering of grammatical classes
+if ! [ -z ${GRAM_CONF_FILE} ] && [ -r ${GRAM_CONF_FILE} ]; then
+	. ${GRAM_CONF_FILE}
+else
+	echo "Cannot find grammatical class clustering configuration file!"
+	exit -1
+fi
+
+# Copy the database, to provide isolation between stages.
+if [[ $STORAGE_NODE = "(RocksStorageNode"* ]]; then
+	cp -pr ${MST_DB} ${GRAM_DB}
+elif [[ $STORAGE_NODE = "(PostgresStorageNode"* ]]; then
+	createdb -T ${MST_DB} ${GRAM_DB}
+else
+	echo "Unknown storage medium!"
+	exit -1
+fi
+
+echo duuude done for now
+exit 0
+
+# Run the classification cogserver
+guile -l ${COMMON_DIR}/cogserver-gram.scm -c "(sleep 150000000)" &
+
+# Wait for the cogserver to intialize.
+sleep 3
+
+# Example. Don't do this unless you mean it.
+echo "(gram-classify-greedy-fuzz 0.65 0.3 4)" | nc $HOSTNAME $PORT >> /dev/null
+echo "(do-stuff-xxxx)" | nc $HOSTNAME $PORT >> /dev/null
+
+# Shut down the server.
+echo Done clustering
+echo "(exit-server)" | nc $HOSTNAME $PORT >> /dev/null
+
+# Wait for the shutdown to complete.
+sleep 1
+
+echo Done
 # ------------------------
