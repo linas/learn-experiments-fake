eec63fa3857ca74d0d0b31ae6573218af39bf070
Your Name <you@example.com>
2021-03-21 22:47:10 -0500

    Re-export

--- expt-13/config/5-export-conf.sh
+++ expt-13/config/5-export-conf.sh
@@ -13,12 +13,12 @@ export PROMPT="scheme@(export)"
 export COGSERVER_CONF=${CONFIG_DIR}/5-cogserver/cogserver-export-fake.conf
 
 # Location of the database where the grammatical classes are held
-export EXPORT_DB=${ROCKS_DATA_DIR}/gram-1a.rdb
+export EXPORT_DB=${ROCKS_DATA_DIR}/gram-4.rdb
 export STORAGE_NODE="(RocksStorageNode \"rocks://${EXPORT_DB}\")"
 
 # Path to the file that will hold the Link Grammar dicationary.
 # The file itself *must* be named `dict.db`, or else LG will fail.
-export LG_DICT_EXPORT=${TEXT_DIR}/learned/dict.db
+export LG_DICT_EXPORT=${TEXT_DIR}/learned-gram-4b/dict.db
 
 # The locale to apply to the dictionary.
 export LG_DICT_LOCALE=EN_us
